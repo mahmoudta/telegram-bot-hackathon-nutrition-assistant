caaddb1e31885ed3ea8101a6c5e2eecb2d7c2268
WasseemB <the.waswas@gmail.com>
2020-02-13 01:54:02 +0200

    some fixes

--- event_scheduler.py
+++ event_scheduler.py
@@ -9,11 +9,12 @@ s = sched.scheduler(time.time, time.sleep)
 
 def do_something(sc):
     chat_ids = Dao.get_all_user_id()
-    for id in chat_ids:
-        res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, "Been a while since last time"))
-        res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, "Below is your summary so far"))
-        user_info = {"id": id}
-        res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, handle_all_info("", user_info)))
+    if chat_ids:
+        for id in chat_ids:
+            res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, "Been a while since last time"))
+            res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, "Below is your summary so far"))
+            user_info = {"id": id}
+            res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, handle_all_info("", user_info)))
 
     # print("hello it is me")
     # do your stuff
