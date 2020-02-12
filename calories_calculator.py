0cf264905515b555f217f825f5825ad05e42bd3a
WasseemB <the.waswas@gmail.com>
2020-02-12 16:11:04 +0200

    some cleaning

--- calories_calculator.py
+++ calories_calculator.py
@@ -1,23 +1,27 @@
 from math import sqrt
 
+
 def calculate_daily_calories(height, weight, age, gender, exercise):
     ''' height: 'cm'\n weight: 'kg'\n age: 'year'\n gender:['M', 'F']'''
-    
+
     exercise_lifestyle = {
-        'low': 1.25 ,
+        'low': 1.25,
         'medium': 1.575,
         'high': 1.85
     }
-    
+
     if gender == 'M':
-        return (13.397*weight + 4.799*height - 5.677*age + 88.362) * exercise_lifestyle[exercise]
+        return (13.397 * weight + 4.799 * height - 5.677 * age + 88.362) * exercise_lifestyle[exercise]
     elif gender == 'F':
-        return  (9.247*weight + 3.098*height - 4.330*age + 447.593) * exercise_lifestyle[exercise]
-    else: return None
+        return (9.247 * weight + 3.098 * height - 4.330 * age + 447.593) * exercise_lifestyle[exercise]
+    else:
+        return None
+
 
 def calculate_bmi(weight, height):
     ''' weight(kg)\n height(cm)\n '''
-    return weight/ (height * height)*(10000)
+    return weight / (height * height) * (10000)
+
 
 def get_adults_bmi_status(bmi):
     if bmi < 16:
@@ -34,8 +38,8 @@ def get_adults_bmi_status(bmi):
         return 'Obese Class I'
     elif bmi < 40:
         return 'Obese Class II'
-    else: return 'Obese Class III'
-
+    else:
+        return 'Obese Class III'
 
 
 if __name__ == "__main__":
