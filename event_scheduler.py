e0dbc0b99b2f00b297ec83f6f203abae80c980c0
WasseemB <the.waswas@gmail.com>
2020-02-13 05:07:35 +0200

    event

--- event_scheduler.py
+++ event_scheduler.py
@@ -2,31 +2,43 @@ import sched, time
 import Dao
 import requests
 from connect_to_bot import TELEGRAM_SEND_MESSAGE_URL
-from command_handler import handle_all_info, get_percentage_calories
+from command_handler import handle_all_info, get_percentage_calories, handle_get_calories, handle_today_protein
 import time
+import random
 
 s = sched.scheduler(time.time, time.sleep)
+
+random_data = ["Been a while since last time\n Below is a summary of your daily report\n",
+               "Did you enter your last meal?\n Use /consume to add it\n",
+               "Check your food for fats before eating them using /check\n",
+               "Have you passed your target calories??\n"
+               "Is your protein intake enough for today??\n"]
 
 
 def do_something(sc):
     chat_ids = Dao.get_all_user_id()
     if chat_ids:
         for id in chat_ids:
-            res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, "Been a while since last time"))
-            res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, "Below is your summary so far"))
             user_info = {"id": id}
-            res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, handle_all_info("", user_info)))
-            # calories_percentage = get_percentage_calories(user_info)
-            #
-            # pre = "Daily Calories "
-            # progress_bar = "█" * int(int(calories_percentage) / 100 * 10)
-            # progress_left = " " * int((100 - int(calories_percentage)) / 100 * 10)
-            # post = "|"
-            # total = f"{pre}{progress_bar}{progress_left}{post} %"
-            # res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, total))
+            random_int = random.randrange(0, len(random_data))
+            if random_int == 0:
+                res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, random_data[0]))
+                res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, handle_all_info("", user_info)))
+            elif random_int == 1:
+                res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, random_data[1]))
+            elif random_int == 2:
+                res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, random_data[2]))
+            elif random_int == 3:
+                res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, random_data[3]))
+                res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, handle_get_calories(" ", user_info)))
+            elif random_int == 4:
+                res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, random_data[4]))
+                res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, handle_today_protein(" ",user_info)))
 
-    # print("hello it is me")
-    # do your stuff
+            # res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, "Been a while since last time"))
+            # res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, "Below is your summary so far"))
+            # res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, handle_all_info("", user_info)))
+
     s.enter(10, 1, do_something, (sc,))
 
 
